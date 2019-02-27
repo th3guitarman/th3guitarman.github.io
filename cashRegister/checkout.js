a3d0f53ca280463959c3c1faf7738bfea443a622
Aaeron Jhonson <aaeronjhonsonwhyte@gmail.com>
2019-02-27 15:54:40 -0500

    files and framework
    added some images and edits to html and js

---
+++ cashRegister/checkout.js
@@ -0,0 +1,24 @@
+var cartItem = [
+  { name:"Meliodas' Sword",
+    use:"Modeled after a broken blade of legend. Nigh unusable.",
+    price:"2000"
+  },
+  { name:"Boar Head Shield",
+    use:"Boars are said to bring fortune and wealth. This boar will at least keep your health.",
+    price:"1500"
+  },
+  { name:"Tusk Spear",
+    use:"Ancient giant boar tusks remain durable and sharp for millenia.",
+    price:"2600"
+  },
+  { name:"First Aid Kit",
+    use:"A bandages, cleaning solution, painkillers, and other first aid amenities.",
+    price:"400"
+  },
+  { name:"Ivory Cider",
+    use:"The secret ingredient of this crisp, robust drink is well guarded.",
+    price:"500"
+  }
+];
+
+document.getElementById('name5').innerHTML= cartItem[4].name;
