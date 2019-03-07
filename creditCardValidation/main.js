4dcafcd05cbf2d67d903307b59bd6e3f3256350f
Aaeron Jhonson <aaeronjhonsonwhyte@gmail.com>
2019-03-07 14:18:02 -0500

    added styling, finalized and organized logic, added in and out-of-console functionality

--- creditCardValidation/main.js
+++ creditCardValidation/main.js
@@ -4,11 +4,26 @@
 //last digit even
 //sum of all digits > 16
 
-function readCard (inputCard) {
-  validity.number = inputCard.toString();
-  inputCard = validity.number.replace(/-/g, '');
+function submitCard () {
+  var inCard = document.getElementById('ccin').value;
+  var inYYYY = document.getElementById('yyin').value, inMM = document.getElementById('mmin').value;
+  var validOut = validateCreditCard(inCard, inMM, inYYYY);
+  var output = '';
 
-  console.log(validity.number + " : " + inputCard);
+  for (var x in validOut) {
+    output += x + ': ' + validOut[x] +'<br />';
+  }
+
+  document.getElementById('r1').innerHTML= output;
+  if (validOut.valid) {
+    document.getElementById('r2').innerHTML= "This is a real card! <br />" +
+    "<div><button type='button' onclick= 'document.location.reload()'>let's try again</button></div>";
+  }
+  else {
+    document.getElementById('r2').innerHTML= "This is just fanta-CC! <br />" +
+    "<div><button type='button' onclick= 'document.location.reload()'>let's try again</button></div>";
+  }
+
 }
 
 function verifyNum(inputCard) {
@@ -53,6 +68,7 @@ function verifyUnique(inputCard) {
     return '';
   }
 }
+
 function verifyLast(inputCard) {
   if ((inputCard[inputCard.length - 1] % 2) != 0) {
     return "odd last digit- ";
@@ -74,11 +90,29 @@ function verifySum(inputCard) {
     return '';
   }
 }
-function validateCreditCard(inputCard) {
+
+function verifyDate(inputMM, inputYYYY) {
+  // var pos_ = inputDate.search('-');
+  // var yy = inputDate.slice(0, pos_);
+  // var mm = inputDate.slice(pos_+1, inputDate.length);
+  // console.log(yy + '-' + mm);
+  var inputDate = inputYYYY + '-' + inputMM;
+  var today = new Date();
+
+  if (today.getTime() > Date.parse(inputDate)) {
+    return "expired- " + inputDate;
+  }
+  else {
+    return '' + inputDate;
+  }
+}
+
+function validateCreditCard(inputCard, inputMM, inputYYYY) {
   //inputCard is a string
   var validity = {
     valid:'',
     number:'',
+    expiry: '',
     error:''
   }
 
@@ -90,12 +124,13 @@ function validateCreditCard(inputCard) {
   validity.error += verifyUnique(inputCard);
   validity.error += verifyLast(inputCard);
   validity.error += verifySum(inputCard);
+  validity.expiry += verifyDate(inputMM, inputYYYY);
 
   if (validity.error == '') {
-    validity.valid = 'true';
+    validity.valid = true;
   }
   else {
-    validity.valid = 'false';
+    validity.valid = false;
   }
 
   return validity;
