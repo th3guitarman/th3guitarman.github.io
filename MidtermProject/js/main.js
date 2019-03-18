b850f3911b6768fccb68f714b987f447a7cc252f
Aaeron <44537579+th3guitarman@users.noreply.github.com>
2019-03-18 18:12:28 -0400

    fixed form function
    also added a pic

---
+++ MidtermProject/js/main.js
@@ -0,0 +1,196 @@
+// var heroData = [
+//   //lucina:
+//   {
+//     name: 'Saint-King Albein Alm Rudolf II',
+//     age: '17',
+//     hometown: 'Rigel Castle',
+//     weapon: 'Sword & Bow',
+//     vocation: 'Hero',
+//     rating: '0',
+//     bio: 'Alm is a kind-hearted yet naïve young man who has a strong sense of justice and subtly idealistic morals. He has a strong sense of obligation, wanting to leave Ram Village to help the people of Zofia when he heard about the outbreak of thieves, famine, and the aggression of the Rigelian Empire for the past few years. His sense of justice is rather black-and-white, as he is quick to blame individuals for a number of follies in situations he does not necessarily have the full context to. He has a vaguely puerile sense of humor, as he loves to make puns, and is a terrible dancer.'
+//   },
+//   {
+//     name: "Anthiese ‘Celica’ Lima",
+//     age: '17',
+//     hometown: 'Zofia Castle',
+//     weapon: 'Sword & Magic',
+//     vocation: 'Princess',
+//     rating: '0',
+//     bio: "Celica is a kind and polite person, who is often bold yet refined, but she holds herself back emotionally to an extent after a series of traumatic events and familial loss; as a result, she can come off as cold and stoic, and normally maintain a aura of polite distance, as shown her body language in cutscenes and sprites, which is subtly guarded and closed-off. She usually regrets showing her raw emotions, especially in the rare moments that she lashes out in anger, bitterness, or even sentiment. Celica feels the need that she has to set aside her emotional candor in order to fulfill her responsibilities as royalty. She is a firm believer in bringing peace without fighting, believing that conflict begets further conflicts. While she prefers solving matters peacefully, she is more than willing to put her life on the line and fight with her comrades for the sake of protecting them."
+//   },
+//   {
+//     name: 'Gray',
+//     age: '18',
+//     hometown: 'Ram Village',
+//     weapon: 'Sword',
+//     vocation: 'Dread Fighter',
+//     rating: '0',
+//     bio: 'Gray has an easygoing, chatty, and sometimes aloof personality. He comes from a merchant family and therefore is knowledgeable of the world outside of Ram. He does not have much self-awareness, however, often barging into conversations and being unconscious of how his own words come off to other people. Gray asserts himself as the tacit "leader" of the villagers and is very confident in his abilities. In battle, he adopts a gung-ho personality which is evident in his quotes and animations. Gray has also been shown to non-maliciously make fun of Alm at times.'
+//   },
+//   {
+//     name: 'Tobin',
+//     age: '17',
+//     hometown: 'Ram Village',
+//     weapon: 'Magic',
+//     vocation: 'Sage',
+//     rating: '0',
+//     bio: "While Tobin possesses a stubborn, serious, and direct personality, he is rather dense, being plagued with social obliviousness, and is poor at reading other people's emotions. Being the eldest of his family, Tobin maintains an air of brotherly warmth, playfulness, and kindness which inevitably transfers to his friends, those of whom he is extremely supportive."
+//   },
+//   {
+//     name: 'Lukas',
+//     age: '24',
+//     hometown: 'Zofia Castle',
+//     weapon: 'Sword',
+//     vocation: 'Dread Fighter',
+//     rating: '0',
+//     bio: "While he can come off as strict in his professionalism, Lukas is sweet-natured and polite to everyone he meets, and although subtle, has a dry sense of humor. He has a very well-maintained calm, reliable, and professional front, however, in the rare events that he loses his temper, Lukas's feelings slip past his said front, such an example being when a duke called him a backwater buffoon, causing him to lose his temper and beat him in a duel. He often fears coming across as cold and detached in his composure, the fears of which hold heavy weight on his thoughts, as he bears guilt about his inability to genuinely put his heart into his own performance as a soldier, and it feels strange and weird to him as a consequence. This ends up being his biggest strength, as he is able to stay calm at the direst situations, even when learning disastrous news."
+//   },
+//   {
+//     name: 'Boey',
+//     age: '16',
+//     hometown: 'Novis Isle',
+//     weapon: 'Bow',
+//     vocation: 'Bow Knight',
+//     rating: '0',
+//     bio: 'Boey, growing up in an impoverished household, is very insecure of himself, but because he values his impression on others, he tends to talk in formal speech mannerisms and highly of himself to overcompensate his image. However, he is more bark than bite, and is terrified easily. As a result of said cowardice, Boey does not actively enjoy engaging in battles. He speaks in a very calm, refined, yet often condescending manner, however, he has been shown to act rather moody. Due to his efforts being focused into said overcompensation of abilities in his speech mannerisms, he is not adept at confronting his own feelings while hiding behind his serious and mature front.'
+//   },
+//   {
+//     name: 'Deen',
+//     age: '30',
+//     hometown: 'Zofian Province',
+//     weapon: 'Brave Sword',
+//     vocation: 'Dread Fighter',
+//     rating: '0',
+//     bio: "He doesn't do small talk."
+//   },
+//   {
+//     name: 'Sonya',
+//     age: '25',
+//     hometown: 'Rigelian Countryside',
+//     weapon: 'Black Magic',
+//     vocation: 'Priestess',
+//     rating: '0',
+//     bio: 'Apparently burdened with an awful past that she hates to talk about, Sonya gives off a cool and unyielding personality and oozes womanly charm. However, internally, the feelings of hatred she hides are violent and tumultuous, and at the time when she faced off with Jedah, she fought against him as though venting her anger and fury.'
+//   },
+//   {
+//     name: 'Est',
+//     age: '16',
+//     hometown: 'Macedon',
+//     weapon: "Sword & Magic",
+//     vocation: 'Priestess',
+//     rating: '0',
+//     bio: "As the youngest of the three Whitewing sisters from Archanea, Est is pure and innocent and often gets told off by her sisters. Though she is cheerful and chatty with a straight-forward personality, the repeated kidnappings take a toll on Est's self-esteem. It is strongly implied that the reason she disappears after the War of Heroes is because she believes that Abel, her husband, would be better off without her. Though she often gets into trouble, Est regularly mentions that she wants to stop being a warrior, settle down, and run a shop with her husband."
+//   },
+//   {
+//     name: 'Genny',
+//     age: '15',
+//     hometown: 'Novis Isle',
+//     weapon: 'White Magic & Black Magic',
+//     vocation: 'Saint',
+//     rating: '0',
+//     bio: 'She is a soft spoken girl, and was initially quite frightened of traveling across Valentia with Celica. Genny seems to have a fondness of men older than her, she states that this is due to a feeling of being taken care of that an older man gives her. Genny has a dislike of her mother for abandoning her at the priory, it is possible this abandonment is what has led to her desire to feel taken care of. She is uncomfortable and nervous around people that remind her of her mother.'
+//   }
+// ];
+
+var heroData = {
+  //lucina:
+  alm: {
+    name: 'Saint-King Albein Alm Rudolf II',
+    age: '17',
+    hometown: 'Rigel Castle',
+    weapon: 'Sword & Bow',
+    vocation: 'Hero',
+    rating: '225',
+    bio: 'Alm is a kind-hearted yet naïve young man who has a strong sense of justice and subtly idealistic morals. He has a strong sense of obligation, wanting to leave Ram Village to help the people of Zofia when he heard about the outbreak of thieves, famine, and the aggression of the Rigelian Empire for the past few years. His sense of justice is rather black-and-white, as he is quick to blame individuals for a number of follies in situations he does not necessarily have the full context to. He has a vaguely puerile sense of humor, as he loves to make puns, and is a terrible dancer.'
+  },
+  celica: {
+    name: "Anthiese ‘Celica’ Lima",
+    age: '17',
+    hometown: 'Zofia Castle',
+    weapon: 'Sword & Magic',
+    vocation: 'Princess',
+    rating: '224',
+    bio: "Celica is a kind and polite person who is often bold yet refined, but she holds herself back emotionally after a series of traumatic events and familial loss; as a result, she can come off as cold and stoic while maintaining a aura of polite distance. She usually regrets showing her raw emotions, especially in the rare moments that she lashes out in anger, bitterness, or even sentiment. Celica feels that she has to set aside her emotional candor in order to fulfill her responsibilities as royalty. She is a firm believer in bringing peace without fighting, believing that conflict begets further conflicts. While she prefers solving matters peacefully, she is more than willing to put her life on the line and fight with her comrades for the sake of protecting them."
+  },
+  gray: {
+    name: 'Gray',
+    age: '18',
+    hometown: 'Ram Village',
+    weapon: 'Sword',
+    vocation: 'Dread Fighter',
+    rating: '241',
+    bio: 'Gray has an easygoing, chatty, and sometimes aloof personality. He comes from a merchant family and therefore is knowledgeable of the world outside of Ram Village. He does not have much self-awareness, however, often barging into conversations and being unconscious of how his own words come off to other people. Gray asserts himself as the tacit "leader" of the villagers and is very confident in his abilities. In battle, he adopts a gung-ho personality which is evident in his quotes and animations. Gray has also been shown to non-maliciously make fun of Alm at times.'
+  },
+  tobin: {
+    name: 'Tobin',
+    age: '17',
+    hometown: 'Ram Village',
+    weapon: 'Excalibur',
+    vocation: 'Sage',
+    rating: '245',
+    bio: "While Tobin possesses a stubborn, serious, and direct personality, he is rather dense, being plagued with social obliviousness, and is poor at reading other people's emotions. Being the eldest of his family, Tobin maintains an air of brotherly warmth, playfulness, and kindness which inevitably transfers to his friends, those of whom he is extremely supportive."
+  },
+  lukas: {
+    name: 'Lukas',
+    age: '24',
+    hometown: 'Zofia Castle',
+    weapon: 'Zweihander',
+    vocation: 'Dread Fighter',
+    rating: '0',
+    bio: "While he can come off as strict in his professionalism, Lukas is sweet-natured and polite to everyone he meets, and although subtle, has a dry sense of humor. He has a very well-maintained calm, reliable, and professional front, however, he has been been known to have a temper - such an example being when a duke called him a backwater buffoon causing him to beat him in a duel. He often fears coming across as cold and detached in his composure, the fears of which hold heavy weight on his thoughts, as he bears guilt about his inability to genuinely put his heart into his own performance as a soldier, and it feels strange and weird to him as a consequence. This ends up being his biggest strength, as he is able to stay calm at the direst situations, even when learning disastrous news."
+  },
+  boey: {
+    name: 'Boey',
+    age: '16',
+    hometown: 'Novis Isle',
+    weapon: 'Killer Bow',
+    vocation: 'Bow Knight',
+    rating: '0',
+    bio: 'Boey, growing up in an impoverished household, is very insecure of himself, but because he values his impression on others, he tends to talk in formal speech mannerisms and highly of himself to overcompensate his image. However, he is more bark than bite, and is terrified easily. As a result of said cowardice, Boey does not actively enjoy engaging in battles. He speaks in a very calm, refined, yet often condescending manner, however, he has been shown to act rather moody. Due to his efforts being focused into said overcompensation of abilities in his speech mannerisms, he is not adept at confronting his own feelings while hiding behind his serious and mature front.'
+  },
+  deen: {
+    name: 'Deen',
+    age: '30',
+    hometown: 'Zofian Province',
+    weapon: 'Brave Sword',
+    vocation: 'Dread Fighter',
+    rating: '0',
+    bio: "He doesn't do small talk."
+  },
+  sonya: {
+    name: 'Sonya',
+    age: '25',
+    hometown: 'Rigelian Countryside',
+    weapon: 'Ladyblade',
+    vocation: 'Priestess',
+    rating: '0',
+    bio: 'Apparently burdened with an awful past that she hates to talk about, Sonya gives off a cool and unyielding personality and oozes womanly charm. However, internally, the feelings of hatred she hides are violent and tumultuous, and at the time when she faced off with Jedah, she fought against him as though venting her anger and fury.'
+  },
+  est: {
+    name: 'Est',
+    age: '16',
+    hometown: 'Macedon',
+    weapon: "Estoc",
+    vocation: 'Priestess',
+    rating: '277',
+    bio: "As the youngest of the three Whitewing sisters from Archanea, Est is pure and innocent and often gets told off by her sisters. Though she is cheerful and chatty with a straight-forward personality, having been repeatedly kidnapped has taken a toll on Est's self-esteem. It is strongly implied that the reason she disappears after the War of Heroes is because she believes that Abel, her husband, would be better off without her. Though she often gets into trouble, Est regularly mentions that she wants to stop being a warrior, settle down, and run a shop with her husband."
+  },
+  genny: {
+    name: 'Genny',
+    age: '15',
+    hometown: 'Novis Isle',
+    weapon: 'Physic & Seraphim',
+    vocation: 'Saint',
+    rating: '0',
+    bio: 'She is a soft spoken girl, and was initially quite frightened of traveling across Valentia with Celica. Genny seems to have a fondness of men older than her, she states that this is due to a feeling of being taken care of that an older man gives her. Genny has a dislike of her mother for abandoning her at the priory, it is possible this abandonment is what has led to her desire to feel taken care of. She is uncomfortable and nervous around people that remind her of her mother.'
+  },
+  new: {
+    name: '',
+    age: '',
+    hometown: '',
+    weapon: '',
+    vocation: '',
+    rating: '',
+    bio: ''
+  }
+};
