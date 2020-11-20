777654483d54452d4885e4cd904dfa8cfd626fbd
th3guitarman <aaeronjhonsonwhyte@gmail.com>
2020-11-20 13:26:23 -0500

    remove inColor

--- mySite/js/script.js
+++ mySite/js/script.js
@@ -1,37 +1,37 @@
-var project_info =  {
-  future_maker: {
+var project_data =  {
+  0: {
     name: 'My Fortune Teller Project, \"Future Maker\"',
     tag: 'Look to Sky3\'s clouds!',
     info: 'Ever struggled to know the direction for your future? So many options! How can one be certain? Find out once and for all which path to choose.',
-    description: 'This was the first class project I had ever attempted. I threw together a few colors I liked for the visuals, but I really had fun playing with how the RNG decided your fate. I decided on a randomizer with case statements. If I were to revisit this project, I'd add more options and an animation for the clouds. I also think that the ability to save your responses in order to "see different futures" wouldn't be an unwelcome addition.',
+    description: 'This was the first class project I had ever attempted. I threw together a few colors I liked for the visuals, but I really had fun playing with how the RNG decided your fate. I decided on a randomizer with case statements. If I were to revisit this project, I\'d add more options and an animation for the clouds. I also think that the ability to save your responses in order to \"see different futures\" wouldn\'t be an unwelcome addition.',
     img: 'mySite/imgs/anew.png',
     link: 'fortuneTeller/index.html'
   },
-  fanta_cc: {
+  1: {
     name: 'My Credit Card Validation Project, \"Is it just Fanta-CC?\"',
     tag: 'Is THIS your card?',
     info: 'Can you get past our machines? Test and see if you can get your computer to think a fake credit card number is real... It only accepts real numbers!',
-    description: 'This project tasked us with simulating credit card verification to follow certain conditions. I recall having trouble checking for and disallowing NaN characters. Having discovered the logic to make it work was a really fulfilling moment for me. It's why I love JS. I think this project was the one that really highlighted for me the importance of function organization. P.S. Peep the pun.',
+    description: 'This project tasked us with simulating credit card verification to follow certain conditions. I recall having trouble checking for and disallowing NaN characters. Having discovered the logic to make it work was a really fulfilling moment for me. It\'s why I love JS. I think this project was the one that really highlighted for me the importance of function organization. P.S. Peep the pun.',
     img: 'creditCardValidation/imgs/purple_data.jpg',
     link: 'creditCardValidation/index.html'
   },
-  boar_hat: {
+  2: {
     name: 'My Cash Register Project, \"Boar Hat\"',
     tag: 'Come Shop at the Boar\'s Hat',
-    info: 'The perfect pit stop before your next adventure! Shop for every adventurer's travel essentials at Meliodas\' shop.',
+    info: 'The perfect pit stop before your next adventure! Shop for every adventurer\'s travel essentials at Meliodas\' shop.',
     description: 'I took the creative liberty of theming this cash register project after a video game shop inspired by an anime which I enjoyed that happened to serve another great pun based on the then-celebrated Chinese New Year. I definitely need to go back in there and DRY that code up, though.',
     img: 'cashRegister/picsrc/boarHat.jpg',
     link: 'cashRegister/index.html'
   },
-  labyrinth_legends: {
+  3: {
     name: 'My Midterm Project, \"Labyrinth Legends\"',
     tag: 'Enter the Labyrinth, if you dare!',
     info: 'Join Lucina, Alm, Celica and the rest of the team in their quest to destory the newest threat - Thabes Labyrinth.',
-    description: 'In this project, we had to edit a website by replacing the content and repairing functionality. I had a LOT of ideas for this site, but only the features you see made the deadline. I was able to edit a lot of content, and implement some interesting script tricks without interfering too much with the CSS. I learned about how to consider one's own strengths and motivations when working against time. Also, Fire Emblem is a great game with a rich history, and this site barely scratches the surface; please go play it.',
+    description: 'In this project, we had to edit a website by replacing the content and repairing functionality. I had a LOT of ideas for this site, but only the features you see made the deadline. I was able to edit a lot of content, and implement some interesting script tricks without interfering too much with the CSS. I learned about how to consider one\'s own strengths and motivations when working against time. Also, Fire Emblem is a great game with a rich history, and this site barely scratches the surface; please go play it.',
     img: 'MidtermProject/img/SOV_banner.jpg',
     link: 'MidtermProject/labyrinthlegends.html'
   },
-  wheel_of_fortune: {
+  4: {
     name: 'My Hackathon Entry Submission, \"Wheel of Fortune\"',
     tag: '',
     info: '',
@@ -39,7 +39,7 @@ var project_info =  {
     img: '',
     link: ''
   },
-  n: {
+  5: {
     name: '',
     tag: '',
     info: '',
@@ -48,3 +48,5 @@ var project_info =  {
     link: ''
   }
 };
+
+//Prevent scrolling
